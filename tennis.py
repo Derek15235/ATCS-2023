95c7d85de2374029ddf7cb9ca9b3a0ef1dbfbe53
Derek15235 <derek.jain@menloschool.org>
2023-12-04 17:26:07 -0800

    Added "out of bounds" and changed naming of racquets

--- tennis.py
+++ tennis.py
@@ -6,7 +6,7 @@ import math
 # Constants
 WIDTH, HEIGHT = 600, 800
 BALL_RADIUS = 8
-PADDLE_WIDTH, PADDLE_HEIGHT = 80, 80
+RACQUET_WIDTH, RACQUET_HEIGHT = 80, 80
 FPS = 60
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -29,7 +29,7 @@ class Ball:
         self.direction = [math.cos(angle), math.sin(angle)]
 
 
-class Paddle:
+class Racquet:
     def __init__(self, x, y, width, height, speed):
         self.x = x
         self.y = y
@@ -75,29 +75,32 @@ class Game:
         pygame.init()
         self.WIDTH = width
         self.HEIGHT = height
+        self.court_width = width - 200
+        self.court_height = height - 200
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption("Vertical Tennis Game")
 
         # Load player images and scale them down
-        self.player1_image = pygame.image.load("player1.png")
-        self.player1_image = pygame.transform.scale(self.player1_image, (int(PADDLE_WIDTH * 7 / 8), int(PADDLE_HEIGHT * 7 / 8)))
+        self.ai_image = pygame.image.load("player1.png")
+        self.ai_image = pygame.transform.scale(self.ai_image, (int(RACQUET_WIDTH * 7 / 8), int(RACQUET_HEIGHT * 7 / 8)))
 
-        self.player2_image = pygame.image.load("player2.png")
-        self.player2_image = pygame.transform.scale(self.player2_image, (int(PADDLE_WIDTH * 7 / 8), int(PADDLE_HEIGHT * 7 / 8)))
+        self.player_image = pygame.image.load("player2.png")
+        self.player_image = pygame.transform.scale(self.player_image, (int(RACQUET_WIDTH * 7 / 8), int(RACQUET_HEIGHT * 7 / 8)))
 
         # Load background image (scaled down)
         background_original = pygame.image.load("tennis_court.png")
-        background_original = pygame.transform.scale(background_original, (WIDTH - 200, HEIGHT - 200))
-        self.background = pygame.Surface((WIDTH, HEIGHT))  # Smaller size
+        background_original = pygame.transform.scale(background_original, (self.court_width, self.court_height))
+        self.background = pygame.Surface((WIDTH, HEIGHT))
         self.background.blit(background_original, (100, 100))
 
         self.clock = pygame.time.Clock()
         self.yellow_box = None
         self.hit_player = False
+        self.current = None
 
         self.ball = Ball(width // 2, height // 2, BALL_RADIUS, 5)
-        self.paddle1 = Paddle(width // 2 - PADDLE_WIDTH // 2, 0, PADDLE_WIDTH, PADDLE_HEIGHT, 10)
-        self.paddle2 = Paddle(width // 2 - PADDLE_WIDTH // 2, height - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT, 10)
+        self.racquet_ai = Racquet(width // 2 - RACQUET_WIDTH // 2, 0, RACQUET_WIDTH, RACQUET_HEIGHT, 10)
+        self.racquet_player = Racquet(width // 2 - RACQUET_WIDTH // 2, height - RACQUET_HEIGHT, RACQUET_WIDTH, RACQUET_HEIGHT, 10)
 
         self.score = [0, 0]
 
@@ -105,39 +108,60 @@ class Game:
         self.delaying = False
         self.delay_timer = 0
 
+
         # Fonts
         self.font = pygame.font.Font(None, 36)
+
+         # Define designated boxes for each player
+        self.ai_box = pygame.Rect(self.WIDTH// 4, self.HEIGHT// 2 , self.WIDTH// 2, self.court_height // 2)
+        self.player_box = pygame.Rect(self.WIDTH // 4, 100, self.WIDTH // 2, self.court_height // 2)
+        # Colors
+        self.GREEN = (0, 255, 0)
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN and self.yellow_box is None:
+            elif event.type == pygame.MOUSEBUTTONDOWN and self.yellow_box is None and self.hit_player:
                 # Create a yellow box at the mouse click position
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-                self.yellow_box = YellowBox(mouse_x, mouse_y, 20, 20, 120)
+
+                if (self.current == "ai" and self.ai_box.collidepoint(mouse_x, mouse_y)) or (self.current == "player" and self.player_box.collidepoint(mouse_x, mouse_y)):
+                    self.yellow_box = YellowBox(mouse_x, mouse_y, 20, 20, 120)
+                else:
+                    # Some altered/added code: yellow box is drawn, displaying its out
+                    self.yellow_box = YellowBox(mouse_x, mouse_y, 20, 20, 120)
+                    self.yellow_box.draw(self.screen)
+                    pygame.display.flip()
+                    pygame.time.delay(1000)
+                    self.yellow_box = None
+                    if self.current == "ai":
+                        self.score[1] += 1
+                    else:
+                        self.score[0] += 1
+                    self.reset_positions()
 
     def update(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_LEFT] and self.paddle1.x > 0:
-            self.paddle1.move_left()
-        if keys[pygame.K_RIGHT] and self.paddle1.x < self.WIDTH - self.paddle1.width:
-            self.paddle1.move_right()
-        if keys[pygame.K_UP] and self.paddle1.y > 0:
-            self.paddle1.move_up()
-        if keys[pygame.K_DOWN] and self.paddle1.y < self.HEIGHT // 2 - self.paddle1.height:
-            self.paddle1.move_down()
+        if keys[pygame.K_LEFT] and self.racquet_ai.x > 0:
+            self.racquet_ai.move_left()
+        if keys[pygame.K_RIGHT] and self.racquet_ai.x < self.WIDTH - self.racquet_ai.width:
+            self.racquet_ai.move_right()
+        if keys[pygame.K_UP] and self.racquet_ai.y > 0:
+            self.racquet_ai.move_up()
+        if keys[pygame.K_DOWN] and self.racquet_ai.y < self.HEIGHT // 2 - self.racquet_ai.height:
+            self.racquet_ai.move_down()
 
-        if keys[pygame.K_a] and self.paddle2.x > 0:
-            self.paddle2.move_left()
-        if keys[pygame.K_d] and self.paddle2.x < self.WIDTH - self.paddle2.width:
-            self.paddle2.move_right()
-        if keys[pygame.K_w] and self.paddle2.y > self.HEIGHT // 2:
-            self.paddle2.move_up()
-        if keys[pygame.K_s] and self.paddle2.y < self.HEIGHT - self.paddle2.height:
-            self.paddle2.move_down()
+        if keys[pygame.K_a] and self.racquet_player.x > 0:
+            self.racquet_player.move_left()
+        if keys[pygame.K_d] and self.racquet_player.x < self.WIDTH - self.racquet_player.width:
+            self.racquet_player.move_right()
+        if keys[pygame.K_w] and self.racquet_player.y > self.HEIGHT // 2:
+            self.racquet_player.move_up()
+        if keys[pygame.K_s] and self.racquet_player.y < self.HEIGHT - self.racquet_player.height:
+            self.racquet_player.move_down()
 
         if self.delaying:
             self.delay_timer += 1
@@ -169,21 +193,23 @@ class Game:
             # Ball collisions with paddles
             if (
                 not self.delaying
-                and self.paddle1.x < self.ball.x < self.paddle1.x + self.paddle1.width
-                and self.paddle1.y < self.ball.y < self.paddle1.y + self.paddle1.height
+                and self.racquet_ai.x < self.ball.x < self.racquet_ai.x + self.racquet_ai.width
+                and self.racquet_ai.y < self.ball.y < self.racquet_ai.y + self.racquet_ai.height
             ):
                 # If the ball hits player1, stop the ball and wait for a click
                 self.ball.direction = [0, 0]
                 self.hit_player = True
+                self.current = "ai"
 
             elif (
                 not self.delaying
-                and self.paddle2.x < self.ball.x < self.paddle2.x + self.paddle2.width
-                and self.paddle2.y < self.ball.y < self.paddle2.y + self.paddle2.height
+                and self.racquet_player.x < self.ball.x < self.racquet_player.x + self.racquet_player.width
+                and self.racquet_player.y < self.ball.y < self.racquet_player.y + self.racquet_player.height
             ):
                 # If the ball hits player2, stop the ball and wait for a click
                 self.ball.direction = [0, 0]
                 self.hit_player = True
+                self.current = "player"
 
             # Ball out of bounds
             if self.ball.y <= 0:
@@ -201,10 +227,10 @@ class Game:
             
 
     def reset_positions(self):
-        self.paddle1.x = self.WIDTH // 2 - PADDLE_WIDTH // 2
-        self.paddle1.y = 0
-        self.paddle2.x = self.WIDTH // 2 - PADDLE_WIDTH // 2
-        self.paddle2.y = self.HEIGHT - PADDLE_HEIGHT
+        self.racquet_ai.x = self.WIDTH // 2 - RACQUET_WIDTH // 2
+        self.racquet_ai.y = 0
+        self.racquet_player.x = self.WIDTH // 2 - RACQUET_WIDTH // 2
+        self.racquet_player.y = self.HEIGHT - RACQUET_HEIGHT
         self.ball.x = self.WIDTH // 2
         self.ball.y = self.HEIGHT // 2
         self.ball.direction = [0, 0]  # Stop the ball initially
@@ -217,15 +243,15 @@ class Game:
         # Draw background
         self.screen.blit(self.background, (0, 0))
 
-         # Draw red borders around the screen
+        # Draw red borders around the screen
         pygame.draw.rect(self.screen, (255, 0, 0), (0, 0, self.WIDTH, 5))                   # Top border
         pygame.draw.rect(self.screen, (255, 0, 0), (0, self.HEIGHT - 5, self.WIDTH, 5))  # Bottom border
         pygame.draw.rect(self.screen, (255, 0, 0), (0, 0, 5, self.HEIGHT))                # Left border
         pygame.draw.rect(self.screen, (255, 0, 0), (self.WIDTH - 5, 0, 5, self.HEIGHT))  # Right border
 
         # Draw player images and ball
-        self.screen.blit(self.player1_image, (self.paddle1.x, self.paddle1.y))
-        self.screen.blit(self.player2_image, (self.paddle2.x, self.paddle2.y))
+        self.screen.blit(self.ai_image, (self.racquet_ai.x, self.racquet_ai.y))
+        self.screen.blit(self.player_image, (self.racquet_player.x, self.racquet_player.y))
         pygame.draw.circle(self.screen, WHITE, (int(self.ball.x), int(self.ball.y)), self.ball.radius)
 
         # Draw the score
@@ -236,9 +262,9 @@ class Game:
         if self.yellow_box:
             self.yellow_box.draw(self.screen)
 
-        # Update the display
-        pygame.display.flip()
-
+        # Draw green outlines for designated boxes
+        pygame.draw.rect(self.screen, WHITE, self.ai_box, 2)
+        pygame.draw.rect(self.screen, WHITE, self.player_box, 2)
 
         # Update the display
         pygame.display.flip()
